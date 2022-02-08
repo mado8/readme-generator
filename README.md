dbd8fa2ee3cdd7e822e35c719740b222cdfe544c
mado <madelineadonley@gmail.com>
2022-02-07 20:24:02 -0700

    fixed bug that prevented file load

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# readme-generator
-A generator to create a descriptive README to be used for future projects as a means of efficiency.
+
+    # readme-generator
+    
+    ## Description
+    This readme generator allows the user to quickly create a descriptive README file using node and inquirer js.
+    
+    ## Table of Contents
+    [ Description. ](#Description)
+    [ Installation. ](#Installation)
+    [ Usage. ](#Usage)
+    [ Contribute. ](#Contribute)
+    [ Tests. ](#Tests)
+    [ Questions. ](#Questions)
+    
+    ## Installation
+    Clone the repository. From there, open the terminal and type "node assets/js/index.js to run the application.
+    
+    ## Usage
+    Follow the instructions from there to create a new readme.
+
+    ##License
+    mit
+    
+    ## Contribute
+    If you would like to contribute, feel free to clone this repository and add changes from there.
+    
+    ## Tests
+    No tests available.
+    
+    ## Questions
+    Visit my other projects https://github.com/mado8
+    Contact me at madelineadonley@gmail.com
+  
--- assets/js/generateMarkdown.js
+++ assets/js/generateMarkdown.js
@@ -1,20 +1,29 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+            'afl-3.0','apache-2.0','artistic-2.0', 'bsl-1.0', 'bsd-2-clause',
+            'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0',
+            'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0',
+            'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 
+            'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib'
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.title}`;
 }
 
 module.exports = generateMarkdown;
